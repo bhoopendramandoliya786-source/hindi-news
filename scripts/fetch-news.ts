4d13ac9870b6288e38802cf34b32f58f89dd70f0
bhoopendramandoliya786-source <bhoopendramandoliya786@gmail.com>
2026-09-13 23:56:37 +0530

    Run student taxonomy cleanup before automated sync

--- scripts/fetch-news.ts
+++ scripts/fetch-news.ts
@@ -1,6 +1,9 @@
+import { seedCategories } from "../lib/category-seeder";
 import { saveIndiaNews } from "../lib/news-saver";
 
-saveIndiaNews()
+Promise.resolve()
+  .then(() => seedCategories())
+  .then(() => saveIndiaNews())
   .then((result) => {
     console.log(JSON.stringify(result));
     process.exit(0);
